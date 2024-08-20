00cc6c4edc10abfebfd2159986e8e52046026a6c
UttU28 <utsavmaan28@gmail.com>
2024-08-20 15:26:27 -0500

    JSK

--- test.py
+++ test.py
@@ -1,9 +1,28 @@
 import pandas as pd
+import numpy as np
 
-df = pd.read_csv('foll.csv')
-filteredDF = df[df['Age'] >= 65]
-# filteredDF.to_csv('filtered_file.csv', index=False)
+allSeniorPatients = pd.read_csv('allSeniorPatients.csv')
+allPatientCurrent = pd.read_csv('allPatientCurrent.csv')
 
-newDF = filteredDF[['First Name', 'Last Name', 'Record Id', 'DOB', 'Email', 'Mobile Phone', 'Last Visit Date']].copy()
-newDF.columns = ['First Name', 'Last Name', 'Record ID', 'DOB', 'Email', 'Mobile Phone', 'Last Visit Date']
-newDF.to_csv('SeniorsNotVisited24M.csv', index=False)
+allSeniorPatients = allSeniorPatients[['First Name', 'Last Name', 'Record Id', 'DOB', 'Email', 'Mobile Phone']].copy()
+allSeniorPatients.columns = ['First Name', 'Last Name', 'Record Id', 'DOB', 'Email', 'Mobile Phone']
+
+allPatientCurrent = allPatientCurrent[allPatientCurrent['Age'] > 64]
+allPatientCurrent = allPatientCurrent[['Record Id', 'Last Visit Date']].copy()
+allPatientCurrent.columns = ['Record Id', 'Last Visited']
+
+allSeniorPatients = pd.merge(allSeniorPatients, allPatientCurrent, on='Record Id', how='left')
+
+allSeniorPatients['Last Visited'] = pd.to_datetime(allSeniorPatients['Last Visited'], errors='coerce')
+
+mask_2023_2024 = allSeniorPatients['Last Visited'].dt.year.isin([2023, 2024])
+mask_2022 = allSeniorPatients['Last Visited'].dt.year == 2022
+mask_2021 = allSeniorPatients['Last Visited'].dt.year == 2021
+mask_before_2021 = allSeniorPatients['Last Visited'].dt.year < 2021
+mask_nan = allSeniorPatients['Last Visited'].isna()
+
+allSeniorPatients[mask_2023_2024].to_csv('allSeniorPatients_2023_2024.csv', index=False)
+allSeniorPatients[mask_2022].to_csv('allSeniorPatients_2022.csv', index=False)
+allSeniorPatients[mask_2021].to_csv('allSeniorPatients_2021.csv', index=False)
+allSeniorPatients[mask_before_2021].to_csv('allSeniorPatients_before_2021.csv', index=False)
+allSeniorPatients[mask_nan].to_csv('allSeniorPatients_nan.csv', index=False)
