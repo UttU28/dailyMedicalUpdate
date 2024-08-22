c3f89e24be3973e5b68e3cd7ce848abf83e50a32
UttU28 <utsavmaan28@gmail.com>
2024-08-21 19:53:29 -0500

    JSK

---
+++ patientData.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+allSeniorPatients = pd.read_csv('allSeniorPatients.csv')
+allPatientCurrent = pd.read_csv('allPatientCurrent.csv')
+
+allSeniorPatients = allSeniorPatients[['First Name', 'Last Name', 'Record Id', 'DOB', 'Email', 'Mobile Phone']].copy()
+allSeniorPatients.columns = ['First Name', 'Last Name', 'Record Id', 'DOB', 'Email', 'Mobile Phone']
+
+allPatientCurrent = allPatientCurrent[allPatientCurrent['Age'] > 64]
+allPatientCurrent = allPatientCurrent[['Record Id', 'Last Visit Date']].copy()
+allPatientCurrent.columns = ['Record Id', 'Last Visited']
+
+allSeniorPatients = pd.merge(allSeniorPatients, allPatientCurrent, on='Record Id', how='left')
+
+allSeniorPatients['Last Visited'] = pd.to_datetime(allSeniorPatients['Last Visited'], errors='coerce')
+
+mask_2023_2024 = allSeniorPatients['Last Visited'].dt.year.isin([2023, 2024])
+mask_2022 = allSeniorPatients['Last Visited'].dt.year == 2022
+mask_2021 = allSeniorPatients['Last Visited'].dt.year == 2021
+mask_before_2021 = allSeniorPatients['Last Visited'].dt.year < 2021
+mask_nan = allSeniorPatients['Last Visited'].isna()
+
+allSeniorPatients[mask_2023_2024].to_csv('SeniorPatients_2023_2024.csv', index=False)
+allSeniorPatients[mask_2022].to_csv('SeniorPatients_2022.csv', index=False)
+allSeniorPatients[mask_2021].to_csv('SeniorPatients_2021.csv', index=False)
+allSeniorPatients[mask_before_2021].to_csv('SeniorPatients_before_2021.csv', index=False)
+allSeniorPatients[mask_nan].to_csv('SeniorPatientsNotVisited.csv', index=False)
--- test.py
+++ test.py
@@ -1,28 +1,65 @@
-import pandas as pd
-import numpy as np
+import re
+import os
+import json
+from docx import Document
 
-allSeniorPatients = pd.read_csv('allSeniorPatients.csv')
-allPatientCurrent = pd.read_csv('allPatientCurrent.csv')
+def read_docx(file_path):
+    doc = Document(file_path)
+    paragraphs = []
+    for para in doc.paragraphs:
+        cleaned_text = para.text.strip()
+        if cleaned_text:
+            paragraphs.append(cleaned_text)
+    return paragraphs
 
-allSeniorPatients = allSeniorPatients[['First Name', 'Last Name', 'Record Id', 'DOB', 'Email', 'Mobile Phone']].copy()
-allSeniorPatients.columns = ['First Name', 'Last Name', 'Record Id', 'DOB', 'Email', 'Mobile Phone']
+def sanitizeFilename(inputString):
+    forbiddenChars = r'[<>:"/\\|?*]'
+    safeString = re.sub(forbiddenChars, '_', inputString)
+    safeString = safeString.strip()
+    return safeString
 
-allPatientCurrent = allPatientCurrent[allPatientCurrent['Age'] > 64]
-allPatientCurrent = allPatientCurrent[['Record Id', 'Last Visit Date']].copy()
-allPatientCurrent.columns = ['Record Id', 'Last Visited']
+def load_existing_data(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    return {}
 
-allSeniorPatients = pd.merge(allSeniorPatients, allPatientCurrent, on='Record Id', how='left')
+def save_data(file_path, data):
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
 
-allSeniorPatients['Last Visited'] = pd.to_datetime(allSeniorPatients['Last Visited'], errors='coerce')
+if __name__ == "__main__":
+    fileDate = 'April 1'
+    docx_file_path = f'ChartWatch/{fileDate}.docx'
+    json_file_path = 'stock_data.json'
+    
+    dateKey = fileDate.split(" ")[-1]
+    monthKey = fileDate.split(" ")[0]
+    allLines = read_docx(docx_file_path)
+    isStockFound = False
+    stockData = load_existing_data(json_file_path)
+    
+    if monthKey not in stockData:
+        stockData[monthKey] = {}
+    for i in range(len(allLines)-1):
+        if allLines[i+1] == "Quarterly Chart:":
+            stockName = sanitizeFilename(allLines[i])
+            if stockName not in stockData:
+                stockData[monthKey][stockName] = {}
+            isStockFound = True
+        elif isStockFound and any(req in allLines[i] for req in ["Quarterly Chart:", "Monthly Chart:", "8-Day Chart:", "Weekly Chart:", "3-Day Chart:", "Daily Chart:", "233 Chart:", "144 Chart:"]):
+            chartName = allLines[i][:-7]
+            
+            if chartName not in stockData[monthKey][stockName]:
+                stockData[monthKey][stockName][chartName] = {}
+            
+            # Check if the date key already exists and if so, update it
+            if dateKey in stockData[monthKey][stockName][chartName]:
+                print(f"Data for {stockName} - {chartName} on {dateKey} already exists and will be updated.")
+            
+            stockData[monthKey][stockName][chartName][dateKey] = allLines[i+1]
+            if "144" in allLines[i]:
+                isStockFound = False
 
-mask_2023_2024 = allSeniorPatients['Last Visited'].dt.year.isin([2023, 2024])
-mask_2022 = allSeniorPatients['Last Visited'].dt.year == 2022
-mask_2021 = allSeniorPatients['Last Visited'].dt.year == 2021
-mask_before_2021 = allSeniorPatients['Last Visited'].dt.year < 2021
-mask_nan = allSeniorPatients['Last Visited'].isna()
-
-allSeniorPatients[mask_2023_2024].to_csv('SeniorPatients_2023_2024.csv', index=False)
-allSeniorPatients[mask_2022].to_csv('SeniorPatients_2022.csv', index=False)
-allSeniorPatients[mask_2021].to_csv('SeniorPatients_2021.csv', index=False)
-allSeniorPatients[mask_before_2021].to_csv('SeniorPatients_before_2021.csv', index=False)
-allSeniorPatients[mask_nan].to_csv('SeniorPatientsNotVisited.csv', index=False)
+    save_data(json_file_path, stockData)
+    print("Data updated and saved successfully.")
